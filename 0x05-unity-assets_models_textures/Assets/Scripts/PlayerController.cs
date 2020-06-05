bab87d5a89a486339254fd449aa7b04c1ba0e8d5
Jeniffervp <737@holbertonschool.com>
2020-06-05 00:22:52 -0500

    [add] movement

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+
+
+    public void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direc = new Vector3(horizontal, 0f, vertical);
+
+        if (direc.magnitude >= 0.1f)
+        {
+            controller.Move(direc * speed * Time.deltaTime);
+        }
+    }
+}
