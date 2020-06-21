2f3f7ad4ca303fc5d7f27e258ea27374860ba2e5
Jeniffervp <737@holbertonschool.com>
2020-06-20 19:28:49 -0500

    [add] main menu script to select scenes

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene("Level0" + level);
+    }
+}
