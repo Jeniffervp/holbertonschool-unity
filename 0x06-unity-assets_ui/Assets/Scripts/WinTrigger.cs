b62bc1b0695d4a1a1e05561aca2856a29955ea08
Jeniffervp <737@holbertonschool.com>
2020-06-25 20:36:26 -0500

    [add] win menu

--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -7,14 +7,23 @@ public class WinTrigger : MonoBehaviour
 {
     public GameObject player;
     public Text timeText;
+    public GameObject timerMenu;
+    public GameObject menuWin;
+    public Text finalText;
+
     // Update is called once per frame
     public void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
             player.GetComponent<Timer>().enabled = false;
-            timeText.color = Color.green;
-                            timeText.fontSize = 80;
+            Win();
         }
+    }
+    public void Win()
+    {
+        timerMenu.SetActive(false);
+        menuWin.SetActive(true);
+        finalText.text = timeText.text;
     }
 }
