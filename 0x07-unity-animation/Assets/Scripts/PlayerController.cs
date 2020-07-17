bc4e1e7e87f4eceeb84e163cb71b8d3ca28c47ad
Jeniffer Vanegas <jeniffervanegas.habi.co>
2020-07-16 21:40:31 -0500

    [Add] intro level 2 and 3

--- 0x07-unity-animation/Assets/Scripts/PlayerController.cs
+++ 0x07-unity-animation/Assets/Scripts/PlayerController.cs
@@ -2,39 +2,70 @@
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
-    public float gravity = -9.81f;
-    public float jumpForce = 1.8f;
+    public float gravity = -20f;
+    public float jumpForce = 5f;
+    public float speed = 12f;
+    public Animator anim;
+    int fallingDown = Animator.StringToHash("Fall");
+    int jump = Animator.StringToHash("Jump");
+    bool run = false;
     Transform cameraMain;
     Transform positionPlayer;
-    public float speed = 12f;
-    public float turnSoftTime = 0.3f;
-    float turnSoftVelocity;
     Vector3 velocity;
+
+
     void Start()
     {
         positionPlayer = GetComponent<Transform>();
         cameraMain = Camera.main.transform;
     }
     public void Update()
-    {
+    {        
         if (controller.isGrounded && velocity.y < 0)
             velocity.y = -2f;
+
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            run = false;
             velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+            anim.SetTrigger(jump);
+            if (positionPlayer.position.y < -5f)
+            {
+                anim.SetTrigger(fallingDown);
+            }
+
+        }
+
+        if (run == true)
+            anim.SetBool("Running", true);
+        else
+            anim.SetBool("Running", false);
+            
         float moveHorizontal = Input.GetAxisRaw("Horizontal");
         float moveVertical = Input.GetAxisRaw("Vertical");
         Vector3 direct = new Vector3(moveHorizontal, 0f, moveVertical).normalized;
         if (direct.magnitude >= 0.1f)
         {
+            run = true;
             float angTarget = Mathf.Atan2(direct.x, direct.z) * Mathf.Rad2Deg + cameraMain.eulerAngles.y;
-            float ang = Mathf.SmoothDampAngle(transform.eulerAngles.y, angTarget, ref turnSoftVelocity, turnSoftTime);
-            transform.rotation = Quaternion.Euler(0f, ang, 0f);
+            transform.rotation = Quaternion.Euler(0f, angTarget, 0f);
             Vector3 moveDirect = Quaternion.Euler(0f, angTarget, 0f) * Vector3.forward;
             controller.Move(moveDirect.normalized * speed * Time.deltaTime);
         }
+        else
+            run = false;
+
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-        if (positionPlayer.position.y < -30f)
-            positionPlayer.position = new Vector3(0, 50, 0);
+
+        if (positionPlayer.position.y < -5f)
+        {
+            anim.SetTrigger(fallingDown);
+            if (positionPlayer.position.y < -50f)
+            {
+                run = false;
+                positionPlayer.position = new Vector3(0, 70, 0);
+            }
+        }
     }
 }
