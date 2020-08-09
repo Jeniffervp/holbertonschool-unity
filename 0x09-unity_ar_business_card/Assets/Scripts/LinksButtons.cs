528e342c627b8f6a60e7f8afd16a5f119f97139c
Jeniffer Vanegas <jeniffervanegas.habi.co>
2020-08-09 17:10:14 -0500

    [Add] business card

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/LinksButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksButtons : MonoBehaviour
+{
+  public void Twitter()
+  {
+    Application.OpenURL("https://twitter.com/jeniffervaneg");
+  }
+  public void Github()
+  {
+    Application.OpenURL("https://github.com/Jeniffervp");
+  }
+  public void Email()
+  {
+    Application.OpenURL("mailto:jeniffervpinedo@gmail.com");
+  }
+  public void LinkedIn()
+  {
+    Application.OpenURL("https://www.linkedin.com/in/jeniffer-vanegas-pinedo/");
+  }
+}
